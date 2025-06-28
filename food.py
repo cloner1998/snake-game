a49a0459ac6feb766b0f355665f5917352c44944
cloner1998 <sajjadyami77@gmail.com>
2025-06-28 21:48:15 +0330

    foods have their unique image

--- food.py
+++ food.py
@@ -2,13 +2,18 @@ import pygame
 from pygame import Vector2
 
 
-class Food():
+class Food:
     def __init__(self):
         self.position = Vector2(5, 6)
 
     def draw(self):
         from game_loop import cell_size
+
         food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size )
+
         from game_loop import screen
         from game_loop import DARK_GREEN
-        pygame.draw.rect(screen, DARK_GREEN, food_rect)
+
+        # pygame.draw.rect(screen, DARK_GREEN, food_rect)
+        food_surface = pygame.image.load("images/food.png")
+        screen.blit( food_surface, food_rect)

c92dddb6a53dbf141bdaed84590b395707125901
cloner1998 <sajjadyami77@gmail.com>
2025-06-28 21:51:47 +0330

    having food in random positions in screen

--- food.py
+++ food.py
@@ -4,7 +4,7 @@ from pygame import Vector2
 
 class Food:
     def __init__(self):
-        self.position = Vector2(5, 6)
+        self.position = self.generate_random_position()
 
     def draw(self):
         from game_loop import cell_size
@@ -17,3 +17,11 @@ class Food:
         # pygame.draw.rect(screen, DARK_GREEN, food_rect)
         food_surface = pygame.image.load("images/food.png")
         screen.blit( food_surface, food_rect)
+
+    def generate_random_position(self):
+        from random import randint
+        from game_loop import num_of_cells
+        x = randint(0, num_of_cells -1 )
+        y = randint(0, num_of_cells -1 )
+        position = Vector2(x, y)
+        return position

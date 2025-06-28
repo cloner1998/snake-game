032e56922c0e0bb419e35142a18b6f886486cb38
cloner1998 <sajjadyami77@gmail.com>
2025-06-28 22:31:16 +0330

    implement some constraint on moving snake

--- game_loop.py
+++ game_loop.py
@@ -22,30 +22,30 @@ pygame.display.set_caption("Snake Game")
 clock = pygame.time.Clock()
 
 food = Food()
-snak = Snake()
+snake = Snake()
 SNAKE_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SNAKE_UPDATE, 200)
 
 while True:
     for event in pygame.event.get():
         if event.type == SNAKE_UPDATE:
-            snak.update_snake()
+            snake.update_snake()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                snak.direction = Vector2(0,-1)
-            if event.key == pygame.K_DOWN:
-                snak.direction = Vector2(0,1)
-            if event.key == pygame.K_LEFT:
-                snak.direction = Vector2(-1,0)
-            if event.key == pygame.K_RIGHT:
-                snak.direction = Vector2(1,0)
+            if event.key == pygame.K_UP and snake.direction != Vector2(0,1):
+                snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_DOWN and snake.direction != Vector2(0,-1):
+                snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_LEFT and snake.direction != Vector2(1,0):
+                snake.direction = Vector2(-1, 0)
+            if event.key == pygame.K_RIGHT and snake.direction != Vector2(-1,0):
+                snake.direction = Vector2(1, 0)
 
     screen.fill(GREEN)
     food.draw()
-    snak.draw_snake()
+    snake.draw_snake()
     pygame.display.update()
 
     clock.tick(60)

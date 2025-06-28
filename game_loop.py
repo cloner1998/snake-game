ab08f667dce4178501bd0ccea6b4b71e5a771836
cloner1998 <sajjadyami77@gmail.com>
2025-06-28 19:48:13 +0330

    make screen and game loop

---
+++ game_loop.py
@@ -0,0 +1,23 @@
+import sys
+
+import pygame
+
+'''
+first make a blank canvas
+'''
+pygame.init()
+
+GREEN = (173, 204, 96)
+DARK_GREEN = (43, 51, 24)
+screen = pygame.display.set_mode((750, 750))
+pygame.display.set_caption("Snake Game")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill(GREEN)
+    pygame.display.update()
+    clock.tick(60)

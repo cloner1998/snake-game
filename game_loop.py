23944699d73b512dc097a2220604ee44697645ba
cloner1998 <sajjadyami77@gmail.com>
2025-06-29 01:29:03 +0330

    last commit

--- game_loop.py
+++ game_loop.py
@@ -30,32 +30,33 @@ game = Game()
 SNAKE_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SNAKE_UPDATE, 200)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == SNAKE_UPDATE:
-            game.update()
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if game.state == "STOPPED":
-                game.state = "RUNNING"
-            if event.key == pygame.K_UP and game.snake.direction != Vector2(0,1):
-                game.snake.direction = Vector2(0, -1)
-            if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0,-1):
-                game.snake.direction = Vector2(0, 1)
-            if event.key == pygame.K_LEFT and game.snake.direction != Vector2(1,0):
-                game.snake.direction = Vector2(-1, 0)
-            if event.key == pygame.K_RIGHT and game.snake.direction != Vector2(-1,0):
-                game.snake.direction = Vector2(1, 0)
-    game.check_collision_with_food()
-    screen.fill(GREEN)
-    pygame.draw.rect(screen, DARK_GREEN, (OFF_SET-5,OFF_SET-5, cell_size*num_of_cells + 10, cell_size*num_of_cells + 10), 5)
-    game.draw()
-    title_surface = title_font.render("Snake Game", True, DARK_GREEN)
-    score_surface = title_font.render("Score: " + str(game.score), True, DARK_GREEN)
-    screen.blit(title_surface, (OFF_SET-5, 20))
-    screen.blit(score_surface, (OFF_SET-5, OFF_SET + cell_size * num_of_cells + 10))
-    pygame.display.update()
+if __name__ == "__main__":
+    while True:
+        for event in pygame.event.get():
+            if event.type == SNAKE_UPDATE:
+                game.update()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if game.state == "STOPPED":
+                    game.state = "RUNNING"
+                if event.key == pygame.K_UP and game.snake.direction != Vector2(0,1):
+                    game.snake.direction = Vector2(0, -1)
+                if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0,-1):
+                    game.snake.direction = Vector2(0, 1)
+                if event.key == pygame.K_LEFT and game.snake.direction != Vector2(1,0):
+                    game.snake.direction = Vector2(-1, 0)
+                if event.key == pygame.K_RIGHT and game.snake.direction != Vector2(-1,0):
+                    game.snake.direction = Vector2(1, 0)
+        game.check_collision_with_food()
+        screen.fill(GREEN)
+        pygame.draw.rect(screen, DARK_GREEN, (OFF_SET-5,OFF_SET-5, cell_size*num_of_cells + 10, cell_size*num_of_cells + 10), 5)
+        game.draw()
+        title_surface = title_font.render("Snake Game", True, DARK_GREEN)
+        score_surface = title_font.render("Score: " + str(game.score), True, DARK_GREEN)
+        screen.blit(title_surface, (OFF_SET-5, 20))
+        screen.blit(score_surface, (OFF_SET-5, OFF_SET + cell_size * num_of_cells + 10))
+        pygame.display.update()
 
-    clock.tick(60)
+        clock.tick(60)
